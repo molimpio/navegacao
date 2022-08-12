ccb570610ed5d9fa9ade1effc4bcc23af156377f
Moisés Olimpio <moiza.olimpio@gmail.com>
2022-08-12 06:42:32 -0300

    View Pager navigation

--- app/src/main/java/com/olimpiodev/navegacao/PagerActivity.kt
+++ app/src/main/java/com/olimpiodev/navegacao/PagerActivity.kt
@@ -2,10 +2,18 @@ package com.olimpiodev.navegacao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_tabs.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 class PagerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pager)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val tabsPageAdapter = TabsPagerAdapter(this, supportFragmentManager)
+        viewPager.adapter = tabsPageAdapter
     }
 }
